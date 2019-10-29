b2bde4026d6d265128d9002e5b10e3605035e204
weishl <weishl>
2019-10-29 14:07:09 +0800

    Day 2

--- Day02/leap.py
+++ Day02/leap.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+<<<<<<< HEAD
 def Is_leap(year):
     if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
         return True
@@ -12,3 +13,11 @@ def Is_leap(year):
 if __name__ == '__main__':
     year = int(input("请输入年份"))
     print (Is_leap(year))
+=======
+def is_leap(year):
+    return (year % 4 == 0 and year % 100 !=0 or year % 400 == 0)
+
+if __name__ == '__main__':
+    year = int(input("输入年份"))
+    print (is_leap(year))
+>>>>>>> 8abc950... Day 2
